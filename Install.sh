9c2e786a9ab316befb5a0eb0e2191ac1085abdc2
kbvstmd <kbvstmd@gmail.com>
2021-04-14 10:39:56 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,7 +1,29 @@
-wget http://119.3.41.228:8080/XCNV/data/CDTS_percentile.txt.gz -O ./data/CDTS_percentile.txt.gz
-gunzip ./data/CDTS_percentile.txt.gz
-wget http://119.3.41.228:8080/XCNV/data/hg19_ljb26_all_converted.vcf.gz -O ./data/hg19_ljb26_all_converted.vcf.gz
-gunzip ./data/hg19_ljb26_all_converted.vcf.gz
+if [ -f "./data/CDTS_percentile.txt" ];then
+  filezise=`ls -l ./data/CDTS_percentile.txt | awk '{ print $5}'`
+  if [ "$filezise" = 7644586817 ];then
+	echo "file exists!"
+	else
+	rm -f ./data/CDTS_percentile.txt
+    wget http://119.3.41.228:8080/XCNV/data/CDTS_percentile.txt.gz -O ./data/CDTS_percentile.txt.gz
+    gunzip ./data/CDTS_percentile.txt.gz
+  fi
+  else
+  wget http://119.3.41.228:8080/XCNV/data/CDTS_percentile.txt.gz -O ./data/CDTS_percentile.txt.gz
+  gunzip ./data/CDTS_percentile.txt.gz
+fi
+if [ -f "./data/hg19_ljb26_all_converted.vcf" ];then
+  filezise=`ls -l ./data/hg19_ljb26_all_converted.vcf | awk '{ print $5}'`
+  if [ "$filezise" = 2242773397 ];then
+	echo "file exists!"
+	else
+	rm -f ./data/hg19_ljb26_all_converted.vcf
+	wget http://119.3.41.228:8080/XCNV/data/hg19_ljb26_all_converted.vcf.gz -O ./data/hg19_ljb26_all_converted.vcf.gz
+	gunzip ./data/hg19_ljb26_all_converted.vcf.gz
+  fi
+  else
+  wget http://119.3.41.228:8080/XCNV/data/hg19_ljb26_all_converted.vcf.gz -O ./data/hg19_ljb26_all_converted.vcf.gz
+  gunzip ./data/hg19_ljb26_all_converted.vcf.gz
+fi
 rscript=`which Rscript`
 script_path=`pwd`
 tar zxvf ./tools/bedtools2.tar.gz -C tools
