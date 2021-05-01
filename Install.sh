308a41f8184ed564d09a36a5c82d6f4b4fc9fb34
kbvstmd <kbvstmd@gmail.com>
2021-05-01 21:40:57 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,9 +1,9 @@
 if [ -f "./data/CDTS_percentile.txt" ];then
   filezise=`ls -l ./data/CDTS_percentile.txt | awk '{ print $5}'`
   if [ "$filezise" = 7644586817 ];then
-	echo "file exists!"
-	else
-	rm -f ./data/CDTS_percentile.txt
+        echo "file exists!"
+        else
+        rm -f ./data/CDTS_percentile.txt
     wget -c http://119.3.41.228:8080/XCNV/data/CDTS_percentile.txt.gz -O ./data/CDTS_percentile.txt.gz
     gunzip ./data/CDTS_percentile.txt.gz
   fi
@@ -14,11 +14,11 @@ fi
 if [ -f "./data/hg19_ljb26_all_converted.vcf" ];then
   filezise=`ls -l ./data/hg19_ljb26_all_converted.vcf | awk '{ print $5}'`
   if [ "$filezise" = 2242773397 ];then
-	echo "file exists!"
-	else
-	rm -f ./data/hg19_ljb26_all_converted.vcf
-	wget -c http://119.3.41.228:8080/XCNV/data/hg19_ljb26_all_converted.vcf.gz -O ./data/hg19_ljb26_all_converted.vcf.gz
-	gunzip ./data/hg19_ljb26_all_converted.vcf.gz
+        echo "file exists!"
+        else
+        rm -f ./data/hg19_ljb26_all_converted.vcf
+        wget -c http://119.3.41.228:8080/XCNV/data/hg19_ljb26_all_converted.vcf.gz -O ./data/hg19_ljb26_all_converted.vcf.gz
+        gunzip ./data/hg19_ljb26_all_converted.vcf.gz
   fi
   else
   wget http://119.3.41.228:8080/XCNV/data/hg19_ljb26_all_converted.vcf.gz -O ./data/hg19_ljb26_all_converted.vcf.gz
